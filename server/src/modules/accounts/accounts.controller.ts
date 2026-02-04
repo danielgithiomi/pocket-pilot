d4fa462a1a2f2dd5db936acd2b909b198c961d73
Daniel Githiomi <danielgithiomi@gmail.com>
2026-02-04 21:30:34 +0400

    Added another test endpoint to account modules

--- server/src/modules/accounts/accounts.controller.ts
+++ server/src/modules/accounts/accounts.controller.ts
@@ -1,14 +1,20 @@
-import {Body, Controller, Post} from '@nestjs/common';
-import { AccountsService } from './accounts.service';
+import {Body, Controller, Get, Post} from '@nestjs/common';
+import {AccountsService} from './accounts.service';
 import {type AccountDto} from "./dtos/account.dto";
 
 @Controller('accounts')
 export class AccountsController {
-  constructor(private readonly accountsService: AccountsService) {}
+    constructor(private readonly accountsService: AccountsService) {
+    }
+
+    @Get()
+    getWallets(){
+        return this.accountsService.getAllWallets();
+    }
 
     @Post()
-    createAccount(@Body() body: AccountDto){
-      return this.accountsService.createAccount(body)
+    createAccount(@Body() body: AccountDto) {
+        return this.accountsService.createAccount(body)
     }
 
 }
--- server/src/modules/accounts/accounts.service.ts
+++ server/src/modules/accounts/accounts.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import {DatabaseService} from "../../infrastructure/database/database.service";
 import {AccountDto} from "./dtos/account.dto";
+import {DatabaseService} from "../../infrastructure/database/database.service";
 
 @Injectable()
 export class AccountsService {
 
     constructor(private readonly db: DatabaseService) {}
+
+    getAllWallets(){
+        return this.db.account.findMany({})
+    }
 
     createAccount(data: AccountDto){
         return this.db.account.create({ data })
