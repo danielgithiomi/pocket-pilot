2ca1cdcca971cdc2e9dd8519603117bfa0c7b816
Daniel Githiomi <danielgithiomi@gmail.com>
2026-02-04 20:47:21 +0400

    Complete turbo set up and docker containers

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,7 @@
+import 'dotenv/config'
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [],
