ae03edd28a8464cf1d3e883ec6705a61d4822be5
Daniel Githiomi <danielgithiomi@gmail.com>
2026-02-10 16:33:09 +0400

    Config (swagger) 'PP-008': Initialize frontend project, installed swagger dependencies by creating package.json and installing node modules.

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ConsoleLogger, Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 const CORS_CONFIG: CorsOptions = {
@@ -20,6 +21,13 @@ const LOGGER_CONFIG = new ConsoleLogger({
     prefix: 'Pocket-Pilot',
 });
 
+const SWAGGER_CONFIG = new DocumentBuilder()
+    .setTitle('Pocket Pilot API')
+    .setDescription('Pocket Pilot API Documentation')
+    .setVersion('1.0.0')
+    .addTag('Pocket Pilot')
+    .build();
+
 const logger = new Logger('APP-ROOT');
 
 async function bootstrap() {
@@ -28,12 +36,17 @@ async function bootstrap() {
     app.use(cookieParser());
     app.setGlobalPrefix('api/v1');
 
+    // Swagger Documentation
+    const document = SwaggerModule.createDocument(app, SWAGGER_CONFIG);
+    SwaggerModule.setup('api/v1/docs', app, document);
+
     await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap()
     .then(() => {
         logger.log(`🚀 Access URL: http://localhost:${process.env.PORT}/api/v1`);
+        logger.log(`📚 API Documentation: http://localhost:${process.env.PORT}/api/v1/docs`);
         logger.log(`🚀 Application bootstrapped successfully!`);
     })
     .catch(err => logger.error(`❌ Application failed to bootstrap: ${err}`));
