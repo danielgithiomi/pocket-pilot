dda4fb1fa74ba9213153ec2a849efc865319c549
Daniel Githiomi <danielgithiomi@gmail.com>
2026-02-12 23:59:22 +0400

    Feat (frontend-pages) 'PP-007': Add Register component with basic structure and unit test, update styles and routing adjustments.
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- frontend/web/src/app/app.routes.ts
+++ frontend/web/src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Home} from './pages/home/home';
+import {Login} from './pages/auth/login/login';
+import {Register} from './pages/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/auth',
+    component: Login
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+    component: Register
+  }
+];
