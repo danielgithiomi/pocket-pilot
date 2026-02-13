d0fa5f23f5267d3c05049755bf0c714f84638267
Daniel Githiomi <danielgithiomi@gmail.com>
2026-02-13 11:22:05 +0400

    Config (frontend-config) 'PP-007': Add baseUrl, path aliases for cleaner imports, and update tsconfig files.
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- frontend/web/src/app/app.routes.ts
+++ frontend/web/src/app/app.routes.ts
@@ -1,8 +1,8 @@
+import {Home} from '@pages/home/home';
 import {Routes} from '@angular/router';
-import {Home} from './pages/home/home';
-import {Login} from './pages/auth/login/login';
-import {Register} from './pages/auth/register/register';
-import {NotFound} from './pages/shared/not-found/not-found';
+import {Login} from '@pages/auth/login/login';
+import {Register} from '@pages/auth/register/register';
+import {NotFound} from '@pages/shared/not-found/not-found';
 
 export const routes: Routes = [
   {
