ee5ff46b91aa4918306b5b71297fb631b60ee092
Daniel Githiomi <danielgithiomi@gmail.com>
2026-02-13 11:00:28 +0400

    Feat (frontend-pages) 'PP-007': Add NotFound component with basic structure, routing, and unit test; update styles and refactor global CSS configuration.
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- frontend/web/src/app/app.routes.ts
+++ frontend/web/src/app/app.routes.ts
@@ -2,19 +2,23 @@ import {Routes} from '@angular/router';
 import {Home} from './pages/home/home';
 import {Login} from './pages/auth/login/login';
 import {Register} from './pages/auth/register/register';
+import {NotFound} from './pages/shared/not-found/not-found';
 
 export const routes: Routes = [
   {
-    path: '/',
+    path: '',
     component: Home
   },
   {
-    path: '/auth',
+    path: 'auth',
     component: Login
   },
   {
-    path: '**',
-    redirectTo: '/',
+    path: 'register',
     component: Register
+  },
+  {
+    path: '**',
+    component: NotFound
   }
 ];
