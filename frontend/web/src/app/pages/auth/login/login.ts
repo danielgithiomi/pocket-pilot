4fe7d19c2fdbb7fdb7e33d6272d2ad3890bf58fc
Daniel Githiomi <danielgithiomi@gmail.com>
2026-02-15 17:02:09 +0400

    Feat (frontend-config) 'PP-007': Add dark mode favicon support and refine login page two-factor authentication card UI.

--- frontend/web/src/app/pages/auth/login/login.ts
+++ frontend/web/src/app/pages/auth/login/login.ts
@@ -1,15 +1,15 @@
+import { Auth_Feature } from '@libs/types';
+import { WarningIcon } from '@atoms/icons/Warning';
 import { NgOptimizedImage } from '@angular/common';
 import { APP_FEATURES } from '@constants/auth.constants';
-import { Auth_Feature } from './../../../libs/types/auth.types';
 import { Component, signal, WritableSignal } from '@angular/core';
 import { AuthFeature } from '@layouts/auth/auth-feature/auth-feature';
-import { WarningIcon } from '@components/ui/atoms/icons/Warning';
 
 @Component({
   selector: 'app-login',
   styleUrl: './login.css',
-  imports: [NgOptimizedImage, AuthFeature, WarningIcon],
   templateUrl: './login.html',
+  imports: [NgOptimizedImage, AuthFeature, WarningIcon],
 })
 export class Login {
   protected readonly features: Auth_Feature[] = APP_FEATURES;
