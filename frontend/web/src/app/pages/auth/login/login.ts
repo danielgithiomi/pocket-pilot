da2beefc63a0979770716df8722a3ff872c17d75
Daniel Githiomi <danielgithiomi@gmail.com>
2026-02-15 16:17:45 +0400

    Feat (frontend-icons) 'PP-007': add CorneredWarning and Warning icon components and integrate WarningIcon into the login page.

--- frontend/web/src/app/pages/auth/login/login.ts
+++ frontend/web/src/app/pages/auth/login/login.ts
@@ -3,11 +3,12 @@ import { APP_FEATURES } from '@constants/auth.constants';
 import { Auth_Feature } from './../../../libs/types/auth.types';
 import { Component, signal, WritableSignal } from '@angular/core';
 import { AuthFeature } from '@layouts/auth/auth-feature/auth-feature';
+import { WarningIcon } from '@components/ui/atoms/icons/Warning';
 
 @Component({
   selector: 'app-login',
   styleUrl: './login.css',
-  imports: [NgOptimizedImage, AuthFeature],
+  imports: [NgOptimizedImage, AuthFeature, WarningIcon],
   templateUrl: './login.html',
 })
 export class Login {
