ad763500f7771f5c2f3e6be08faf8cbd7c556ab9
Daniel Githiomi <danielgithiomi@gmail.com>
2026-02-16 18:24:25 +0400

    Feat (frontend-config) 'PP-007': Refactor form schemas, centralize validation logic, and improve feature component bindings
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- frontend/web/src/app/libs/types/auth.types.ts
+++ frontend/web/src/app/libs/types/auth.types.ts
@@ -1,4 +1,73 @@
+import {email, min, required, schema, validate} from '@angular/forms/signals';
+
 export interface Auth_Feature {
   id: number;
   name: string;
 }
+
+// LOGIN
+export interface LoginSchema {
+  email: string;
+  password: string;
+}
+
+export const initialLoginFormState: LoginSchema = {
+  email: '',
+  password: '',
+};
+
+export const loginFormValidationSchema = schema<LoginSchema>(
+  (root) => {
+    // Email
+    email(root.email, { message: "The email address format is invalid!"});
+    required(root.email, { message: "The email address is required field!"});
+
+    // Password
+    required(root.password, { message: "The password is required field!"});
+    min(root.password, 8, { message: "The password cannot be less than 8 characters!"})
+  }
+)
+
+// REGISTRATION
+export interface RegisterSchema {
+  email: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export const initialRegisterFormState: RegisterSchema = {
+  email: '',
+  username: '',
+  password: '',
+  confirmPassword: ''
+};
+
+export const registerFormValidationSchema = schema<RegisterSchema>(
+  (root) => {
+    // Email
+    email(root.email, { message: "The email address format is invalid!"});
+    required(root.email, { message: "The email address is required field!"});
+
+    // Username
+    required(root.username, { message: "The username is required field!"});
+    min(root.username, 6, { message: "The username cannot be less than 6 characters!"});
+
+    // Password
+    required(root.password, { message: "The password is required field!"});
+    min(root.password, 8, { message: "The password cannot be less than 8 characters!"});
+
+    // Confirm Password
+    required(root.confirmPassword, { message: "The confirm password is required field!"});
+    min(root.confirmPassword, 8, { message: "The confirm password cannot be less than 8 characters!"});
+    validate(root.confirmPassword, (context) => {
+      const confirmPassword = context.value();
+      const password = context.valueOf(root.password);
+      if (confirmPassword === password) return null;
+      return {
+        kind: 'password-mismatch',
+        message: "The passwords entered do not match"
+      }
+    })
+  }
+)
--- frontend/web/src/app/pages/auth/register/register.ts
+++ frontend/web/src/app/pages/auth/register/register.ts
@@ -1,47 +1,19 @@
+import {Router} from '@angular/router';
+import {form, FormField} from '@angular/forms/signals';
 import {Component, inject, signal} from '@angular/core';
 import {AuthBranding} from '@layouts/auth/auth-branding/branding';
-import {email, form, FormField, min, required, validate} from '@angular/forms/signals';
-import {Router} from '@angular/router';
+import {initialRegisterFormState, registerFormValidationSchema, RegisterSchema} from '@libs/types';
 
 @Component({
   selector: 'app-register',
-  imports: [AuthBranding, FormField],
-  templateUrl: './register.html',
   styleUrl: './register.css',
+  templateUrl: './register.html',
+  imports: [AuthBranding, FormField],
 })
 export class Register {
-
-  protected registerFormModel = signal<RegisterSchema>(initialFormState);
-
-  protected registerForm = form(
-    this.registerFormModel,
-    (root) => {
-      // Email
-      email(root.email, { message: "The email address format is invalid!"});
-      required(root.email, { message: "The email address is required field!"});
-
-      // Username
-      required(root.username, { message: "The username is required field!"});
-      min(root.username, 6, { message: "The username cannot be less than 6 characters!"});
-
-      // Password
-      required(root.password, { message: "The password is required field!"});
-      min(root.password, 8, { message: "The password cannot be less than 8 characters!"});
-
-      // Confirm Password
-      required(root.confirmPassword, { message: "The confirm password is required field!"});
-      min(root.confirmPassword, 8, { message: "The confirm password cannot be less than 8 characters!"});
-      validate(root.confirmPassword, (context) => {
-        const confirmPassword = context.value();
-        const password = context.valueOf(root.password);
-        if (confirmPassword === password) return null;
-        return {
-          kind: 'password-mismatch',
-          message: "The passwords entered do not match"
-        }
-      })
-    })
-
+  // FORM
+  protected registerFormModel = signal<RegisterSchema>(initialRegisterFormState);
+  protected registerForm = form(this.registerFormModel, registerFormValidationSchema);
   private readonly router = inject(Router);
 
   routeToLogin = () => this.router.navigate(['/auth/login']);
@@ -54,17 +26,3 @@ export class Register {
     console.log(email, username, password);
   }
 }
-
-export interface RegisterSchema {
-  email: string;
-  username: string;
-  password: string;
-  confirmPassword: string;
-}
-
-export const initialFormState: RegisterSchema = {
-  email: '',
-  username: '',
-  password: '',
-  confirmPassword: ''
-};
