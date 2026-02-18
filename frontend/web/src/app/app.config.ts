afd4c421b3d212f438de99ee3524b0b328f79689
Daniel Githiomi <danielgithiomi@gmail.com>
2026-02-18 14:39:13 +0400

    Ref (frontend-config) 'PP-007': Add shared types module, configure global paths, and enhance app config with HTTP client
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- frontend/web/src/app/app.config.ts
+++ frontend/web/src/app/app.config.ts
@@ -1,9 +1,11 @@
 import {routes} from './app.routes';
+import {provideHttpClient, withFetch} from '@angular/common/http';
 import {provideRouter, withComponentInputBinding} from '@angular/router';
 import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withFetch()),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding())
   ]
