6543b288f9c3c4c2b6729ed86016746f527ade2e
Daniel Githiomi <danielgithiomi@gmail.com>
2026-02-20 23:31:47 +0400

    Feat (frontend-services) 'PP-009': introduce generic API client and inject HttpClient into login service

--- frontend/web/src/app/services/auth/login.service.ts
+++ frontend/web/src/app/services/auth/login.service.ts
@@ -1,10 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
-import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
+
+  private readonly http = inject(HttpClient);
+
   login() {
     console.log('Login service');
     console.log(environment.API_BASE_URL);
