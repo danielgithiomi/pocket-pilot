e2b97313e758506ff65aa43e9893916564683be3
Daniel Githiomi <danielgithiomi@gmail.com>
2026-03-11 11:27:30 +0400

    Ref (frontend) 'PP-012': Update drawer component styles and structure, including padding adjustments and z-index changes for improved layout and functionality.

--- frontend/web/src/app/components/structural/main/drawer/drawer.ts
+++ frontend/web/src/app/components/structural/main/drawer/drawer.ts
@@ -1,4 +1,5 @@
 import { ImageDimensions } from '@libs/types';
+import { WEB_ROUTES } from '@global/constants';
 import { Component, signal } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { Chevron } from '@components/ui/atoms/icons';
@@ -13,6 +14,8 @@ export class Drawer {
   protected readonly drawerOpen = signal<boolean>(true);
   protected readonly logoUrl: string = '/images/branding/logo.png';
   protected readonly logoDimensions: ImageDimensions = { width: 70, height: 70 };
+
+  protected readonly links = WEB_ROUTES;
 
   toggleDrawer() {
     this.drawerOpen.set(!this.drawerOpen());
