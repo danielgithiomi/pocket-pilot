77a4d76736b6407be45784640c6b9dc86ecb9bb5
Daniel Githiomi <danielgithiomi@gmail.com>
2026-03-12 07:49:20 +0400

    Feat (web) 'PP-013': Update app header component to include user summary and improve layout structure by adding a vertical separator between icons, enhancing visual clarity and user interaction.

--- frontend/web/src/app/components/structural/headers/app-header/app-header.ts
+++ frontend/web/src/app/components/structural/headers/app-header/app-header.ts
@@ -1,4 +1,5 @@
-import { Component, input, output } from '@angular/core';
+import { UserSummary } from "./user-summary";
+import { Component, output } from '@angular/core';
 import { LucideAngularModule, Menu} from 'lucide-angular';
 import { NotificationBell, SettingsIcon } from '@components/ui/atoms/icons';
 
@@ -6,7 +7,7 @@ import { NotificationBell, SettingsIcon } from '@components/ui/atoms/icons';
   selector: 'app-header',
   styleUrl: './app-header.css',
   templateUrl: './app-header.html',
-  imports: [NotificationBell, SettingsIcon, LucideAngularModule],
+  imports: [NotificationBell, SettingsIcon, LucideAngularModule, UserSummary],
 })
 export class AppHeader {
   protected readonly Menu = Menu;
---
+++ frontend/web/src/app/components/structural/headers/app-header/user-summary.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-summary',
+  imports: [],
+  styles: '',
+  template: `
+    <div>
+      <p>User Summary</p>
+    </div>
+  `,
+})
+export class UserSummary {}
