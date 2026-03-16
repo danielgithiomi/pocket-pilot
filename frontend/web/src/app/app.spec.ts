06d3d65cf068c0519d19656375a2c2698f45865c
Daniel Githiomi <danielgithiomi@gmail.com>
2026-03-16 20:56:30 +0400

    Ref (web-app) 'PP-011': Cleaned up index.html by removing unnecessary whitespace, reordered imports in main.ts for consistency, deleted unused app.css file, and refactored app.ts to remove obsolete styleUrl reference.

--- frontend/web/src/app/app.spec.ts
+++ frontend/web/src/app/app.spec.ts
@@ -1,5 +1,5 @@
-import {TestBed} from '@angular/core/testing';
-import {App} from './app';
+import { App } from './app';
+import { TestBed } from '@angular/core/testing';
 
 describe('App', () => {
   beforeEach(async () => {
--- frontend/web/src/app/app.ts
+++ frontend/web/src/app/app.ts
@@ -1,13 +1,10 @@
+import { Component } from '@angular/core';
 import { ToastContainer } from '@atoms/toast';
 import { RouterOutlet } from '@angular/router';
-import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrl: './app.css',
   templateUrl: './app.html',
   imports: [RouterOutlet, ToastContainer],
 })
-export class App {
-  protected readonly title = signal('web');
-}
+export class App {}
