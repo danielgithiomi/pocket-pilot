f463623e0b8a9e47c3a0668b89088af24fd0fc13
Daniel Githiomi <danielgithiomi@gmail.com>
2026-03-17 18:21:34 +0400

    Feat (web-transactions) 'PP-014': Created the layout for the transactions page

--- frontend/web/src/app/pages/main/transactions/transactions.ts
+++ frontend/web/src/app/pages/main/transactions/transactions.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Button } from '@components/ui/atoms/button';
+import { LucideAngularModule, ListFilterPlus } from 'lucide-angular';
+import { NoData } from '@components/structural/main/no-data/no-data';
 
 @Component({
   selector: 'app-transactions',
-  imports: [],
-  templateUrl: './transactions.html',
   styleUrl: './transactions.css',
+  templateUrl: './transactions.html',
+  imports: [Button, LucideAngularModule, NoData],
 })
-export class Transactions {}
+export class Transactions {
+  protected readonly iconSize: number = 18;
+  protected readonly Plus = ListFilterPlus;
+
+  protected isFormOpen = signal<boolean>(false);
+}
