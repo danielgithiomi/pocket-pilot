7529e1404b511572166258f5cc296330d8743504
Daniel Githiomi <danielgithiomi@gmail.com>
2026-03-17 15:44:33 +0400

    Feat (web-no-data) 'PP-014': Created a reusable no data component

---
+++ frontend/web/src/app/components/structural/main/no-data/no-data.ts
@@ -0,0 +1,26 @@
+import { Component, input } from '@angular/core';
+import { LottieComponent, AnimationOptions } from 'ngx-lottie';
+
+@Component({
+  selector: 'no-data',
+  imports: [LottieComponent],
+  template: `
+    <div class="flex flex-col items-center justify-center">
+      <ng-lottie
+        [options]="options"
+        [width]="animationDimensions()"
+        [height]="animationDimensions()"
+      />
+      <p class="text-center font-medium uppercase text-muted">{{ message() }}</p>
+    </div>
+  `,
+})
+export class NoData {
+  readonly message = input.required<string>();
+  readonly animationDimensions = input<string>('350px');
+  protected readonly options: AnimationOptions = {
+    loop: true,
+    autoplay: true,
+    path: '/animations/no_data.json',
+  };
+}
--- frontend/web/src/app/pages/main/accounts/accounts.ts
+++ frontend/web/src/app/pages/main/accounts/accounts.ts
@@ -1,10 +1,12 @@
 import { Component, computed, inject } from '@angular/core';
 import { AccountsService } from '@api/accounts.service';
+import { NoData } from "@components/structural/main/no-data/no-data";
 
 @Component({
   selector: 'accounts',
   styleUrl: './accounts.css',
   templateUrl: './accounts.html',
+  imports: [NoData],
 })
 export class Accounts {
   protected readonly accountsService = inject(AccountsService);
