45bb96760ee9c469e7ff9bc51d056c90644b0761
Daniel Githiomi <danielgithiomi@gmail.com>
2026-03-19 02:26:02 +0400

    Feat (web-transactions) 'PP-015': Enhanced transaction form functionality by updating button click handler, adding associated account selection, and improving form validation. Refactored TypeScript types to include accountId in transaction schema, ensuring better data management and user experience.

--- frontend/web/src/app/pages/main/transactions/transactions.ts
+++ frontend/web/src/app/pages/main/transactions/transactions.ts
@@ -1,9 +1,12 @@
 import { form } from '@angular/forms/signals';
+import { Input } from '@components/ui/atoms/input';
+import { Select } from '@components/ui/atoms/select';
 import { Button } from '@components/ui/atoms/button';
-import { Component, inject, signal } from '@angular/core';
+import { AccountsService } from '@api/accounts.service';
 import { ToastService } from '@components/ui/atoms/toast';
 import { Form } from '@components/ui/organisms/form/form';
 import { TransactionsService } from '@api/transactions.service';
+import { Component, computed, inject, signal } from '@angular/core';
 import { LucideAngularModule, ListFilterPlus } from 'lucide-angular';
 import { NoData } from '@components/structural/main/no-data/no-data';
 import {
@@ -11,8 +14,6 @@ import {
   initialTransactionFormState,
   transactionFormValidationSchema,
 } from './transactions.types';
-import { Input } from "@components/ui/atoms/input";
-import { Select } from "@components/ui/atoms/select";
 
 @Component({
   selector: 'app-transactions',
@@ -26,15 +27,25 @@ export class Transactions {
 
   // Services
   private readonly toastService = inject(ToastService);
+  private readonly accountsService = inject(AccountsService);
   private readonly transactionsService = inject(TransactionsService);
 
   // Data
+  protected readonly accounts = this.accountsService.getUserAccounts();
   protected readonly transactionTypes = this.transactionsService.getTransactionTypes();
   protected readonly transactionCategories = this.transactionsService.getTransactionCategories();
 
   // States
   protected isFormOpen = signal<boolean>(false);
   protected isSubmitting = signal<boolean>(false);
+
+  // Computed
+  protected accountsDropdown = computed(() => {
+    return this.accounts.value()?.data?.data?.map((account) => ({
+      value: account.id,
+      label: account.name,
+    }));
+  });
 
   // Form
   protected transactionFormModel = signal<TransactionSchema>(initialTransactionFormState);
@@ -46,6 +57,18 @@ export class Transactions {
     this.transactionFormModel.set(initialTransactionFormState);
   }
 
+  protected handleOpenForm() {
+    if (this.accounts.value()?.data?.count === 0) {
+      this.toastService.show({
+        variant: 'info',
+        title: 'No accounts found!',
+        details: 'Please create an account first to add transactions.',
+      });
+      return;
+    }
+    this.isFormOpen.set(true);
+  }
+
   protected handleCloseForm(source: 'icon' | 'overlay') {
     if (source === 'icon') this.resetTransactionForm();
     this.isFormOpen.set(false);
@@ -54,8 +77,13 @@ export class Transactions {
   protected submitTransactionForm(event: Event) {
     event.preventDefault();
     this.isSubmitting.set(true);
-    // TODO: Implement transaction creation
-    this.isSubmitting.set(false);
+
+    const { amount, type, category, accountId } = this.transactionFormModel();
+
+    console.log(amount, type, category, accountId);
+
+    setTimeout(() => {
+      this.isSubmitting.set(false);
+    }, 3000);
   }
 }
-
--- frontend/web/src/app/pages/main/transactions/transactions.types.ts
+++ frontend/web/src/app/pages/main/transactions/transactions.types.ts
@@ -1,17 +1,20 @@
 import { CreateTransactionRequest } from '@global/types';
-import { min, minLength, required, schema, validate } from '@angular/forms/signals';
+import { min, required, schema, validate } from '@angular/forms/signals';
 
-export type TransactionSchema = CreateTransactionRequest;
+export type TransactionSchema = CreateTransactionRequest & {
+  accountId: string;
+};
 
 export const initialTransactionFormState: TransactionSchema = {
-  amount: 0,
   type: '',
+  amount: 0,
   category: '',
+  accountId: '',
 };
 
 export const transactionFormValidationSchema = schema<TransactionSchema>((root) => {
   required(root.amount, { message: 'The amount is required field!' });
-  min(root.amount, 1, { message: 'The minimum transaction amount must be at least 1.00!' });
+  min(root.amount, 1, { message: 'The minimum transaction amount must be at least 1!' });
 
   validate(root.type, (context) => {
     const type = context.value();
