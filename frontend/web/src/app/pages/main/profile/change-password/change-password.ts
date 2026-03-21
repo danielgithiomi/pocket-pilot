215cd3f3b17be71d18d3317207079b32ba50859b
Daniel Githiomi <danielgithiomi@gmail.com>
2026-03-21 12:23:01 +0400

    Ref (web-profile) 'PP-013': Remove unused profile detail and change password components, streamline profile page structure by integrating change password functionality directly into the profile template.

---
+++ frontend/web/src/app/pages/main/profile/change-password/change-password.ts
@@ -0,0 +1,89 @@
+import { Input } from '@atoms/input';
+import { Button } from '@atoms/button';
+import { Component, input, signal } from '@angular/core';
+import {
+  ChangePasswordSchema,
+  changePasswordValidationSchema,
+  initialChangePasswordFormState,
+} from './change-password.types';
+import { form } from '@angular/forms/signals';
+
+@Component({
+  selector: 'change-password',
+  imports: [Input, Button],
+  template: `
+    <div class="flex flex-col justify-between h-full">
+      <div class="card-header">
+        <p class="title">Change your password.</p>
+        <p class="subtitle">Update your password to remain secure.</p>
+      </div>
+
+      <div class="card-content">
+        <form id="change-password-form" (submit)="onSubmitChangePassword()">
+          <!-- Start Hidden username for accessibility -->
+          <input
+            type="text"
+            class="hidden"
+            name="username"
+            [value]="userEmail()"
+            autocomplete="username"
+          />
+          <!-- End of hidden username for accessibility -->
+
+          <atom-input
+            type="password"
+            id="current-password"
+            label="Current Password"
+            placeholder="••••••••"
+            autocomplete="current-password"
+            [formField]="changePasswordForm.currentPassword"
+          />
+
+          <atom-input
+            type="password"
+            id="new-password"
+            label="New Password"
+            placeholder="••••••••"
+            autocomplete="new-password"
+            [formField]="changePasswordForm.newPassword"
+          />
+
+          <atom-input
+            id="confirm-new-password"
+            type="password"
+            label="Confirm New Password"
+            placeholder="••••••••"
+            autocomplete="new-password"
+            [formField]="changePasswordForm.confirmNewPassword"
+          />
+        </form>
+      </div>
+
+      <div class="flex flex-row items-center justify-end w-full mt-6">
+        <atom-button
+          type="submit"
+          label="Change Password"
+          id="submit-change-password"
+          form="change-password-form"
+          [isLoading]="isSubmittingChangePassword()"
+          [disabled]="changePasswordForm().invalid() || isSubmittingChangePassword()"
+        />
+      </div>
+    </div>
+  `,
+})
+export class ChangePassword {
+  // INPUTS
+  userEmail = input.required<string>();
+
+  // STATES
+  protected readonly isSubmittingChangePassword = signal<boolean>(false);
+
+  // FORM
+  protected changePasswordFormModel = signal<ChangePasswordSchema>(initialChangePasswordFormState);
+  protected changePasswordForm = form(this.changePasswordFormModel, changePasswordValidationSchema);
+
+  protected onSubmitChangePassword() {
+    console.log('Submit change password');
+  }
+}
--- frontend/web/src/app/pages/main/profile/profile.ts
+++ frontend/web/src/app/pages/main/profile/profile.ts
@@ -1,37 +1,19 @@
 import { NgClass } from '@angular/common';
-import { form } from '@angular/forms/signals';
 import { AuthService } from '@api/auth.service';
-import { ProfileDetail } from './profile-detail';
-import { Input } from '@components/ui/atoms/input';
-import { Button } from '@components/ui/atoms/button';
 import { DrawerService } from '@infrastructure/services';
-import { Component, computed, inject, signal } from '@angular/core';
-import {
-  ChangePasswordSchema,
-  changePasswordValidationSchema,
-  initialChangePasswordFormState,
-} from './profile.types';
+import { Component, computed, inject } from '@angular/core';
+import { ProfileDetail } from './profile-detail/profile-detail';
+import { ChangePassword } from './change-password/change-password';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.html',
-  imports: [ProfileDetail, NgClass, Input, Button],
+  imports: [ProfileDetail, NgClass, ChangePassword],
 })
 export class Profile {
   protected readonly authService = inject(AuthService);
   protected readonly drawerService: DrawerService = inject(DrawerService);
 
-  // STATES
-  protected readonly isSubmittingChangePassword = signal<boolean>(false);
-
   protected readonly user = this.authService.user()!;
   protected readonly isFetchingProfileData = computed(() => this.authService.isLoading());
-
-  // FORM
-  protected changePasswordFormModel = signal<ChangePasswordSchema>(initialChangePasswordFormState);
-  protected changePasswordForm = form(this.changePasswordFormModel, changePasswordValidationSchema);
-  
-  protected onSubmitChangePassword() {
-    console.log('Submit change password');
-  }
 }
