ee55055b5dbb8c509a92deb31f11d33b9c13a5df
Daniel Githiomi <danielgithiomi@gmail.com>
2026-04-11 16:37:40 +0400

    Feat (bills-form) 'PP-023': Implement new BillsForm component with dynamic form handling for bill creation, including validation and submission logic, enhancing user experience and modularity.

--- frontend/web/src/app/pages/main/goals/bills-form/bills-form.ts
+++ frontend/web/src/app/pages/main/goals/bills-form/bills-form.ts
@@ -1,15 +1,54 @@
-import { Component } from "@angular/core";
-import { NewBillFormValidationSchema } from "./bills-form.types";
+import { Button } from '@atoms/button';
+import { Form } from '@organisms/form';
+import { form } from '@angular/forms/signals';
+import { RadioOption, Radio } from '@atoms/radio';
+import { DatePicker } from '@organisms/date-picker';
+import { LucideAngularModule } from 'lucide-angular';
+import { Component, computed, inject, input, output, signal } from '@angular/core';
+import {
+  NewBillSchema,
+  NewBillFormValidationSchema,
+  INITIAL_FORM_STATE as initialFormData,
+} from './bills-form.types';
 
 @Component({
-    selector: 'app-bills-form',
-    templateUrl: './bills-form.html',
+  selector: 'app-bills-form',
+  templateUrl: './bills-form.html',
+  imports: [LucideAngularModule, Form, Button, Radio, DatePicker],
 })
 export class BillsFormComponent {
+  // Inputs
+  isBillsFormOpen = input.required<boolean>();
 
-    // 
+  // Outputs
+  onBillsFormClose = output<void>();
 
-    // FORM
-    protected readonly newBillFormModel = NewBillFormValidationSchema;
-    
+  // Signals
+  protected readonly isSubmittingForm = signal<boolean>(false);
+
+  // Services
+//   protected readonly billService = inject(BillService);
+
+  // Form
+  protected readonly newBillFormModel = signal<NewBillSchema>(initialFormData);
+  protected readonly newBillForm = form(this.newBillFormModel, NewBillFormValidationSchema);
+
+  // Computed
+//   protected readonly formattedBillTypes = computed<RadioOption[]>(() => {
+//     return Object.values(NewBillSchema.billType.enum).map((type) => ({
+//       value: type,
+//       label: type,
+//     }));
+//   });
+
+  // Methods
+  protected handleCloseForm() {
+    this.onBillsFormClose.emit();
+  }
+
+  // Submissions
+  protected submitNewBillForm(event: Event) {
+    event.preventDefault();
+    console.log(this.newBillForm().value());
+  }
 }
