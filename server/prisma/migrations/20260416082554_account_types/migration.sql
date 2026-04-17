a4c7670e34afaea39c64f58a91c29aa622d6068c
Daniel Githiomi <danielgithiomi@gmail.com>
2026-04-17 15:34:41 +0400

    Ref (prisma, schema) 'PP-024': Update AccountType enum in schema.prisma to include new account types such as LOAN, CRYPTO, DEPOSIT, OVERDRAFT, BROKERAGE, and RETIREMENT, while reorganizing existing types for improved clarity and structure. This enhances the data model for better alignment with application requirements.

---
+++ server/prisma/migrations/20260416082554_account_types/migration.sql
@@ -0,0 +1,14 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "AccountType" ADD VALUE 'LOAN';
+ALTER TYPE "AccountType" ADD VALUE 'CRYPTO';
+ALTER TYPE "AccountType" ADD VALUE 'DEPOSIT';
+ALTER TYPE "AccountType" ADD VALUE 'OVERDRAFT';
+ALTER TYPE "AccountType" ADD VALUE 'BROKERAGE';
+ALTER TYPE "AccountType" ADD VALUE 'RETIREMENT';
