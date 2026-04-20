7761ada07a601203f64869072cbaec27234be15d
Daniel Githiomi <danielgithiomi@gmail.com>
2026-04-20 16:30:58 +0400

    Ref (cache, utils) 'PP-024': Simplify `getOrSetCache` by removing unnecessary console logs. Retain only essential cache checks for cleaner code. Update `converters.ts` to add missing newline for consistency.
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- server/src/common/cache/entiity.cache.ts
+++ server/src/common/cache/entiity.cache.ts
@@ -19,13 +19,8 @@ export abstract class EntityCache<T> {
     async getOrSetCache<V = T>(id: string, callback: () => Promise<V>, ttl: number = this.TTL): Promise<V> {
         const key = this.key(id);
         const cachedValue = await this.cache.get<V>(key);
-        if (cachedValue) {
-            console.log('Cache hit', key);
-            return cachedValue;
-        }
+        if (cachedValue) return cachedValue;
 
-        // No cache found
-        console.log('Cache miss', key);
         const freshValue = await callback();
         await this.cache.set(key, freshValue, secondsToMilliseconds(ttl));
         return freshValue;
