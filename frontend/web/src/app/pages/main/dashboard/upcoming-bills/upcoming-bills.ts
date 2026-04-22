cb6b8c22e83e5714aa274a2681deac41ae679dd4
Daniel Githiomi <danielgithiomi@gmail.com>
2026-04-22 20:25:39 +0400

    Ref (UI, dashboard) 'PP-016': Remove unused CSS files for `calendar` and `loaders`. Update import paths in `styles.css` for better organization. Enhance `upcoming-bills` component with improved error handling and data display logic. Refactor TypeScript to streamline bill fetching and filtering for upcoming due dates.

--- frontend/web/src/app/pages/main/dashboard/upcoming-bills/upcoming-bills.ts
+++ frontend/web/src/app/pages/main/dashboard/upcoming-bills/upcoming-bills.ts
@@ -1,12 +1,17 @@
+import { Bill } from '@global/types';
+import { NgClass } from '@angular/common';
 import { ReceiptCent } from 'lucide-angular';
-import { Component, computed, input } from '@angular/core';
+import { BillsService } from '@api/bills.service';
+import { Component, computed, inject, input } from '@angular/core';
+import { NoData } from '@components/structural/main/no-data/no-data';
 import { DashboardCard } from '@structural/main/dashboard-card/dashboard-card';
 import { BillItem, BillItemSkeleton } from '@pages/main/goals_and_bills/items';
+import { FetchError } from '@components/structural/main/fetch-error/fetch-error';
 
 @Component({
-    selector: 'upcoming-bills',
-    templateUrl: './upcoming-bills.html',
-    imports: [DashboardCard, BillItemSkeleton, BillItem],
+  selector: 'upcoming-bills',
+  templateUrl: './upcoming-bills.html',
+  imports: [DashboardCard, BillItemSkeleton, FetchError, NoData, BillItem, NgClass],
 })
 export class UpcomingBills {
   // ICONS
@@ -17,8 +22,27 @@ export class UpcomingBills {
 
   // SIGNALS
 
+  // SERVICES
+  private readonly billsService = inject(BillsService);
+
+  // DATA
+  protected readonly bills$ = this.billsService.getUserBills();
+
   // COMPUTED
-  protected readonly isFetchingBills = computed(() => {
-    return true;
+  protected readonly isFetchingBills = computed(() => this.bills$.isLoading());
+
+  protected readonly upcomingBills = computed<Bill[]>(() => {
+    const now = new Date();
+    const oneWeekFromNow = new Date();
+    oneWeekFromNow.setDate(now.getDate() + 7);
+
+    const allBills = this.bills$.value()?.data;
+
+    if (!allBills) return [];
+
+    return allBills.filter((bill) => {
+      const billDueDate = new Date(bill.dueDate);
+      return billDueDate >= now && billDueDate <= oneWeekFromNow;
+    });
   });
 }
