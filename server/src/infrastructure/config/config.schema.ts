ff99f6ebf806f586b3eca435d5b5b334ff1dd779
Daniel Githiomi <danielgithiomi@gmail.com>
2026-04-30 23:52:02 +0400

    Feat (AWS) 'PP-026': Enhance AWS Configuration: Add AWS-related environment variables and update config schema and service to include AWS settings for improved integration and error handling.

--- server/src/infrastructure/config/config.schema.ts
+++ server/src/infrastructure/config/config.schema.ts
@@ -14,6 +14,14 @@ export const PPConfigSchema = z.object({
     REDIS_HOST: z.string().default('localhost'),
     REDIS_PORT: z.coerce.number().default(6379),
     REDIS_DEFAULT_TTL_SECONDS: z.coerce.number().int().positive().default(15), // 15 seconds
+
+    // AWS
+    AWS_MAX_ATTEMPTS: z.coerce.number().default(5),
+    AWS_S3_REGION: z.string().default('AWS_S3_REGION'),
+    AWS_MAX_SOCKET_TIMEOUT: z.coerce.number().default(5000),
+    AWS_ACCESS_KEY_ID: z.string().default('AWS_ACCESS_KEY_ID'),
+    AWS_MAX_CONNECTION_TIMEOUT: z.coerce.number().default(5000),
+    AWS_SECRET_ACCESS_KEY: z.string().default('AWS_SECRET_ACCESS_KEY'),
 });
 
 export type PPCustomConfig = z.infer<typeof PPConfigSchema>;
--- server/src/infrastructure/config/config.service.ts
+++ server/src/infrastructure/config/config.service.ts
@@ -9,15 +9,26 @@ export class PPConfigService {
 
     get redis(): IRedisConfig {
         return {
-            port: this.service.get<number>('REDIS_PORT'),
-            host: this.service.get<string>('REDIS_HOST'),
-            defaultTTL: this.service.get<number>('REDIS_DEFAULT_TTL_SECONDS'),
+            port: this.service.getOrThrow<number>('REDIS_PORT'),
+            host: this.service.getOrThrow<string>('REDIS_HOST'),
+            defaultTTL: this.service.getOrThrow<number>('REDIS_DEFAULT_TTL_SECONDS'),
         };
     }
 
     get database(): IDatabaseConfig {
         return {
-            url: this.service.get<string>('DATABASE_URL'),
+            url: this.service.getOrThrow<string>('DATABASE_URL'),
+        };
+    }
+
+    get aws() {
+        return {
+            region: this.service.getOrThrow<string>('AWS_S3_REGION'),
+            maxAttempts: this.service.getOrThrow<number>('AWS_MAX_ATTEMPTS'),
+            accessKeyId: this.service.getOrThrow<string>('AWS_ACCESS_KEY_ID'),
+            socketTimeout: this.service.getOrThrow<number>('AWS_MAX_SOCKET_TIMEOUT'),
+            secretAccessKey: this.service.getOrThrow<string>('AWS_SECRET_ACCESS_KEY'),
+            connectionTimeout: this.service.getOrThrow<number>('AWS_MAX_CONNECTION_TIMEOUT'),
         };
     }
 }
