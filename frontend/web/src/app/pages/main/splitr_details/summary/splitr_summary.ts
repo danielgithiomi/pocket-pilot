7d2848dd506946f16e83ad908c0f2c26a8c72f61
Daniel Githiomi <danielgithiomi@gmail.com>
2026-05-28 19:06:35 +0400

    Ref (splitwise) 'PP-027': Update SplitrSummary component to include detailed summaries of event participants, enhancing data presentation. Modify HTML structure for better layout and integrate input binding for event data. Refactor TypeScript to compute grand totals and individual summaries, improving maintainability and readability.

--- frontend/web/src/app/pages/main/splitr_details/summary/splitr_summary.ts
+++ frontend/web/src/app/pages/main/splitr_details/summary/splitr_summary.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { ISplitrEvent } from '@global/types';
+import { formatCurrency } from '@libs/utils';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
     selector: 'splitr-summary',
-    templateUrl: './splitr_summary.html'
+    templateUrl: './splitr_summary.html',
 })
-
 export class SplitrSummary {
+    // INPUTS
+    readonly splitrEvent = input.required<ISplitrEvent>();
 
+    // COMPUTED
+    protected readonly formattedGrandTotal = computed(() => {
+        const { verificationTotal, billingCurrency } = this.splitrEvent();
+        return formatCurrency(verificationTotal, billingCurrency, 2, true, false);
+    });
+
+    protected readonly consumerSummaries = computed<PayerSummary[]>(() => {
+        const { eventMembers, eventSplittables } = this.splitrEvent();
+
+        const memberTotalAmount = (member: string) =>
+            eventSplittables.reduce(
+                (acc, splittable) =>
+                    acc +
+                    splittable.quantitySplits
+                        .filter((quantitySplit) => quantitySplit.consumerName === member)
+                        .reduce(
+                            (acc, quantitySplit) =>
+                                acc + splittable.unitPrice * quantitySplit.consumerQuantity,
+                            0,
+                        ),
+                0,
+            );
+
+        const totalItemsConsumed = (payer: string) =>
+            eventSplittables.reduce(
+                (acc, splittable) =>
+                    acc +
+                    splittable.quantitySplits
+                        .filter((quantitySplit) => quantitySplit.consumerName === payer)
+                        .reduce((acc, quantitySplit) => acc + quantitySplit.consumerQuantity, 0),
+                0,
+            );
+
+        return eventMembers.map((member) => ({
+            isSettled: false,
+            payerName: member,
+            payerAmount: memberTotalAmount(member),
+            itemsConsumed: totalItemsConsumed(member),
+        }));
+    });
+}
+
+// INTERFACES
+interface PayerSummary {
+    payerName: string;
+    isSettled: boolean;
+    payerAmount: number;
+    itemsConsumed: number;
 }
