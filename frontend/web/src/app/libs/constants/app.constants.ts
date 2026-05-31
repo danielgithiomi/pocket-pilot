e18ed80187c9faff1cf1e588261a019042d4dc88
Daniel Githiomi <danielgithiomi@gmail.com>
2026-05-31 13:21:24 +0400

    Ref (splitwise) 'PP-027': Update color palette in app.constants.ts for enhanced visual consistency and adjust text size in splitr-breakdown.html for improved readability. Change badge label logic to reflect settlement status accurately.

--- frontend/web/src/app/libs/constants/app.constants.ts
+++ frontend/web/src/app/libs/constants/app.constants.ts
@@ -49,12 +49,18 @@ export const AdditionalDrawerNavigationLinks: DrawerNavigationLink[] = [
 export const QUANTITIES: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export const COLOR_PALETTE: ColorPalette[] = [
-    { bg: 'bg-sky-300', fg: 'text-sky-900' },
-    { bg: 'bg-pink-300', fg: 'text-pink-900' },
-    { bg: 'bg-lime-300', fg: 'text-lime-900' },
-    { bg: 'bg-amber-300', fg: 'text-amber-900' },
-    { bg: 'bg-violet-300', fg: 'text-violet-900' },
-    { bg: 'bg-teal-300', fg: 'text-teal-900' },
-    { bg: 'bg-rose-300', fg: 'text-rose-900' },
-    { bg: 'bg-indigo-300', fg: 'text-indigo-900' },
+    { bg: 'bg-red-400', fg: 'text-red-950' },
+    { bg: 'bg-orange-400', fg: 'text-orange-950' },
+    { bg: 'bg-amber-400', fg: 'text-amber-950' },
+    { bg: 'bg-yellow-400', fg: 'text-yellow-950' },
+    { bg: 'bg-lime-400', fg: 'text-lime-950' },
+    { bg: 'bg-green-400', fg: 'text-green-950' },
+    { bg: 'bg-teal-400', fg: 'text-teal-950' },
+    { bg: 'bg-cyan-400', fg: 'text-cyan-950' },
+    { bg: 'bg-blue-400', fg: 'text-blue-950' },
+    { bg: 'bg-indigo-400', fg: 'text-indigo-950' },
+    { bg: 'bg-violet-400', fg: 'text-violet-950' },
+    { bg: 'bg-fuchsia-400', fg: 'text-fuchsia-950' },
+    { bg: 'bg-pink-400', fg: 'text-pink-950' },
+    { bg: 'bg-stone-400', fg: 'text-stone-950' },
 ];
