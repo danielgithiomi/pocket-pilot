47f172e2230f60830b9437c3dc43af5c12fdf591
Daniel Githiomi <danielgithiomi@gmail.com>
2026-06-05 11:05:10 +0400

    Feat (FAQs & Features) 'PP-029': Restructure the FAQs & Features page layout for improved organization and responsiveness. Introduced a feature suggestion section with a button to open a feature form. Updated TypeScript to manage feature form state and integrated a button component for enhanced user interaction.

--- frontend/web/src/app/pages/main/faqs_features/faqs-features.ts
+++ frontend/web/src/app/pages/main/faqs_features/faqs-features.ts
@@ -1,20 +1,25 @@
+import { Button } from "@atoms/button";
 import { NgClass } from '@angular/common';
+import { ContactItem } from '@global/types';
+import { RouterLink } from '@angular/router';
 import { Accordion } from '@molecules/accordion';
-import { Component, computed, inject } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 import { CONTACT_ITEMS, FAQ_ITEMS } from '@global/constants';
+import { Component, computed, inject, signal } from '@angular/core';
 import { DrawerService } from '@infrastructure/services/drawer.service';
-import { RouterLink } from '@angular/router';
-import { ContactItem } from '@global/types';
 
 @Component({
     selector: 'faqs-features',
     templateUrl: './faqs-features.html',
-    imports: [NgClass, LucideAngularModule, Accordion, RouterLink],
+    imports: [NgClass, LucideAngularModule, Accordion, RouterLink, Button],
 })
 export class FaqsFeatures {
     // ICONS
     protected readonly iconSize = 14;
+
+    // SIGNAL STATES
+    protected readonly isFeatureFormOpen = signal<boolean>(false);
+    protected readonly isLoadingFeatures = signal<boolean>(false);
 
     // SERVICES
     protected readonly drawerService = inject(DrawerService);
@@ -24,4 +29,13 @@ export class FaqsFeatures {
     protected readonly contactItems = computed<ContactItem[]>(() =>
         CONTACT_ITEMS.filter((item) => item.id === 'email'),
     );
+
+    // UTILITIES
+    protected handleOpenFeatureForm() {
+        this.isFeatureFormOpen.set(true);
+    }
+
+    protected handleCloseFeatureForm() {
+        this.isFeatureFormOpen.set(false);
+    }
 }
