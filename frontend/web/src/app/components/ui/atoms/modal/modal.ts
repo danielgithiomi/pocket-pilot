df853cc8ff3040f81bacb240422c3ac4043c0e04
Daniel Githiomi <danielgithiomi@gmail.com>
2026-06-17 17:37:15 +0400

    Ref (modals, faqs_features) 'PP-029': Adjust `FeatureDetails` padding for consistent spacing. Refactor `Modal` close icon structure with new wrapper style and smaller icon size for improved usability and alignment.
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- frontend/web/src/app/components/ui/atoms/modal/modal.ts
+++ frontend/web/src/app/components/ui/atoms/modal/modal.ts
@@ -22,7 +22,7 @@ export class Modal {
 
     // ICONS
     protected readonly X = X;
-    protected readonly iconSize = 18;
+    protected readonly iconSize = 16;
     showScrollBar = input<boolean>(false);
     closeModalOnBackdropClick = input.required<boolean>();
 
