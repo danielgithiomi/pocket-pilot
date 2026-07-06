dcf7b346d0619794feb8eabe951d5a8ce48a7c64
Daniel Githiomi <danielgithiomi@gmail.com>
2026-07-06 15:39:56 +0400

    Ref (notifications) 'PP-030': Add mark-all-as-read functionality and improve dropdown behavior
    - Introduced `handleMarkAllAsRead` method in `notifications-dropdown.ts` with loading indicator support.
    - Added `closeNotificationsPanelEvent` output to manage dropdown visibility from the parent component.
    - Updated `notifications-dropdown.html` for event handling, icon state changes, and improved markup structure.
    - Adjusted styles in `notifications-dropdown.css` for better layout consistency and scroll behavior.
    
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- frontend/web/src/app/components/structural/headers/app-header/notifications-dropdown/notifications-dropdown.ts
+++ frontend/web/src/app/components/structural/headers/app-header/notifications-dropdown/notifications-dropdown.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { TabList, TabListItem } from '@atoms/tab-list';
+import { Component, output, signal } from '@angular/core';
 import { CheckCheck, LucideAngularModule, X } from 'lucide-angular';
 
 @Component({
     selector: 'notifications-dropdown',
     styleUrl: './notifications-dropdown.css',
     templateUrl: './notifications-dropdown.html',
-    imports: [LucideAngularModule, TabList]
+    imports: [NgClass, LucideAngularModule, TabList]
 })
 export class NotificationsDropdown {
     // ICONS
@@ -14,23 +15,48 @@ export class NotificationsDropdown {
     protected readonly CloseIcon = X;
     protected readonly MarkAsReadIcon = CheckCheck;
 
+    // OUTPUTS
+    protected closeNotificationsPanelEvent = output<void>();
+
+    // DATA
+    protected readonly notificationsCount = [
+        {
+            name: 'Notification 1',
+            variant: 'read'
+        },
+        {
+            name: 'Notification 2',
+            variant: 'unread'
+        }
+    ];
+
     // SIGNAL STATES
     protected readonly activeTabIndex = signal<number>(0);
+    protected readonly isMarkingAllAsRead = signal<boolean>(false);
 
     // METHODS
     protected handleTabChange(index: number) {
         this.activeTabIndex.set(index);
     }
 
+    protected handleMarkAllAsRead() {
+        alert('Mark all as read');
+        this.isMarkingAllAsRead.set(true);
+
+        setTimeout(() => {
+            this.isMarkingAllAsRead.set(false);
+        }, 2500);
+    }
+
     // DATA
     protected readonly tabListItems: TabListItem[] = [
         {
             value: 'all',
-            label: 'All'
+            label: `All [${this.notificationsCount.length}]`
         },
         {
             value: 'unread',
-            label: 'Unread'
+            label: `Unread [${this.notificationsCount.filter(notification => notification.variant === 'unread').length}]`
         }
     ];
 }
