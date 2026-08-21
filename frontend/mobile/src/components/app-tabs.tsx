7dede5bd2449872f9731e0dcc42307bb1513fa59
Daniel Githiomi <danielgithiomi@gmail.com>
2026-08-21 23:49:59 +0400

    Ref (code restructure) 'PP-032': Refactored the tabs component to import the NativeTabTrigger directly
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- frontend/mobile/src/components/app-tabs.tsx
+++ frontend/mobile/src/components/app-tabs.tsx
@@ -1,7 +1,6 @@
-import { NativeTabs } from 'expo-router/unstable-native-tabs';
-import { useColorScheme } from 'react-native';
-
 import { Colors } from '@/constants/theme';
+import { useColorScheme } from 'react-native';
+import { NativeTabs, NativeTabTrigger } from 'expo-router/unstable-native-tabs';
 
 export default function AppTabs() {
     const scheme = useColorScheme();
@@ -12,16 +11,16 @@ export default function AppTabs() {
             backgroundColor={colors.background}
             indicatorColor={colors.backgroundElement}
             labelStyle={{ selected: { color: colors.text } }}>
-            <NativeTabs.Trigger name="index">
-                <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
-                <NativeTabs.Trigger.Icon src={require('@/assets/images/tabIcons/home.png')} renderingMode="template" />
+            <NativeTabs.Trigger name="index" labelVisibilityMode="selected">
+                <NativeTabTrigger.Label>Home</NativeTabTrigger.Label>
+                <NativeTabTrigger.Icon src={require('@/assets/images/tabIcons/home.png')} renderingMode="template" />
             </NativeTabs.Trigger>
 
-            <NativeTabs.Trigger name="explore">
-                <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
+            <NativeTabs.Trigger name="explore" labelVisibilityMode="selected">
+                <NativeTabTrigger.Label>Explore</NativeTabTrigger.Label>
                 <NativeTabs.Trigger.Icon
-                    src={require('@/assets/images/tabIcons/explore.png')}
                     renderingMode="template"
+                    src={require('@/assets/images/tabIcons/explore.png')}
                 />
             </NativeTabs.Trigger>
         </NativeTabs>
