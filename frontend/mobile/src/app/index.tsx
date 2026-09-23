f539999a1694db829d09771303e9099d0a9c37af
Daniel Githiomi <danielgithiomi@gmail.com>
2026-09-24 00:00:04 +0400

    Ref (imports) 'PP-032': Rearranged index file imports
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- frontend/mobile/src/app/index.tsx
+++ frontend/mobile/src/app/index.tsx
@@ -1,12 +1,11 @@
 import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { AnimatedIcon } from '@/components/animated-icon';
 import { HintRow } from '@/components/hint-row';
+import { WebBadge } from '@/components/web-badge';
+import { Platform, StyleSheet } from 'react-native';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
+import { AnimatedIcon } from '@/components/animated-icon';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
 
 function getDevMenuHint() {
